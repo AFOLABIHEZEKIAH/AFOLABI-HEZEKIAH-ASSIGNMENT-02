20a75f61182e553ce0af738e9ada3db8a8b156c0
AFOLABI, Hezekiah Hezekiah <104587787+AFOLABIHEZEKIAH@users.noreply.github.com>
2022-07-14 19:43:02 +0000

    ATTEMPTED_1,2

--- src/assignment.js
+++ src/assignment.js
@@ -4,15 +4,6 @@ const assignment = {};
 // ========================
 
 
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
 
 function sumOfNumbers(arrayOfNumbers) {
 
@@ -31,15 +22,7 @@ var sum= sumOfNumbers(Numbers)
 console.log(sum)
 
 
-/**
- * Challenge - 2
- * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
+
 function countEvenNumbers(array){
 	var count = 0;
 	for(let i = 0 ; i < array.length; i++) {
